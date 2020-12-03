bc4209e2f86aea1aaa53d4dae122a08e6156d148
Aaron Miller <aaron@aaron-miller.net>
2020-12-02 18:22:46 -0600

    Refactor names for part two

--- 2020/day02.py
+++ 2020/day02.py
@@ -1,4 +1,4 @@
-class Password():
+class MinMaxPasswordValidator():
     def __init__(self, password_str):
         req_str, char_str, password_str = password_str.split(' ')
         self.min = int(req_str.split('-')[0])
@@ -13,14 +13,14 @@ class Password():
             return False
         return True
 
-assert Password("1-3 a: abcde").is_valid()
-assert Password("1-3 b: cdefg").is_valid() == False
-assert Password("2-9 c: ccccccccc").is_valid()
+assert MinMaxPasswordValidator("1-3 a: abcde").is_valid()
+assert MinMaxPasswordValidator("1-3 b: cdefg").is_valid() == False
+assert MinMaxPasswordValidator("2-9 c: ccccccccc").is_valid()
 
 if __name__ == "__main__":
     total_count = 0
     with open('inputs/day02.txt', 'r')as f:
         for line in f.readlines():
-            if Password(line).is_valid():
+            if MinMaxPasswordValidator(line).is_valid():
                 total_count += 1
     print(total_count)
